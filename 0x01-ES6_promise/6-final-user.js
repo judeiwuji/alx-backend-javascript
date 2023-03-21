6e6ca60400a47590a927934fde446e21879a1911
jude iwuji <judeiwuji@gmail.com>
2023-03-21 15:19:48 -0500

    update

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,5 +2,8 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([uploadPhoto(fileName), signUpUser(firstName, lastName)]);
+  return Promise.allSettled([
+    uploadPhoto(fileName),
+    signUpUser(firstName, lastName),
+  ]);
 }
