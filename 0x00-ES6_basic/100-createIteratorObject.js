42ae001d3e627e30d1450742186899b4cc46c842
jude iwuji <judeiwuji@gmail.com>
2023-03-21 10:33:58 -0500

    Fixed lint

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,8 +1,8 @@
 export default function createIteratorObject(report) {
   const noOfDepts = report.getNumberOfDepartments(report.allEmployees);
+  const allEmployees = Object.values(report.allEmployees);
   let deptIdx = 0;
   let empIdx = 0;
-  const allEmployees = Object.values(report.allEmployees);
   let employees = allEmployees[deptIdx];
   let iterCount = 0;
 
@@ -11,23 +11,28 @@ export default function createIteratorObject(report) {
       return {
         next() {
           if (deptIdx < noOfDepts) {
-	  const empLen = employees.length;
-	  if (empIdx >= empLen && deptIdx + 1 < noOfDepts) {
-              employees = allEmployees[++deptIdx];
-              empIdx = 0;
-	  }
+            const empLen = employees.length;
+            let result;
 
-	  if (empIdx < empLen) {
-	    iterCount += 1;
-              return {
-	      value: employees[empIdx++],
-	      done: false,
-	    };
-	  }
+            if (empIdx >= empLen && deptIdx + 1 < noOfDepts) {
+              deptIdx += 1;
+              employees = allEmployees[deptIdx];
+              empIdx = 0;
+            }
+
+            if (empIdx < empLen) {
+              iterCount += 1;
+              result = {
+                value: employees[empIdx],
+                done: false,
+              };
+              empIdx += 1;
+              return result;
+            }
           }
           return {
             value: iterCount,
-	 done: true,
+            done: true,
           };
         },
       };
