5d7d0597c37ec12cbb6ff9cbc0232709ca486831
judeiwuji <judeiwuji@gmail.com>
2023-03-23 15:44:04 -0700

    Add: Task 8

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,37 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this.size = size;
+    this.location = location;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  set size(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Size must be a number');
+    }
+
+    this._size = value;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  set location(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Location must be a number');
+    }
+    this._location = value;
+  }
+
+  toString() {
+    return this.location;
+  }
+
+  valueOf() {
+    return this.size;
+  }
+}
