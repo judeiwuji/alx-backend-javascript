46e63aef20dde5647a3e70b131775b870ff4bd5d
jude iwuji <judeiwuji@gmail.com>
2023-03-28 21:45:45 +0100

    Fixed error

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,10 +1,9 @@
 export const weakMap = new WeakMap();
 export function queryAPI(endpoint) {
-  if (!weakMap.has(endpoint)) {
-    weakMap.set(endpoint, 0);
+  let count = 0;
+  if (weakMap.has(endpoint)) {
+    count = weakMap.get(endpoint);
   }
-
-  const count = weakMap.get(endpoint);
 
   if (count >= 5) {
     throw new Error('Endpoint load is high');
