5b08bf0dafe620fb5d3d9cad7fe1d2759b3d7edb
jude iwuji <judeiwuji@gmail.com>
2023-03-28 21:59:00 +0100

    update

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,8 @@ export default function cleanSet(set, startString) {
   const cleaned = [];
 
   for (const item of set.values()) {
-    if (startString !== '' && item.startsWith(startString)) {
+    const str = item.substring(0, startString.length);
+    if (startString && str === startString) {
       cleaned.push(item.substring(startString.length));
     }
   }
