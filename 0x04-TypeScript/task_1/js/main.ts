98e87824ae7a855fd238a3e8315b254a7ac962bb
judeiwuji <judeiwuji@gmail.com>
2023-03-29 14:16:31 -0700

    Add: Task 4

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -20,3 +20,31 @@ let printTeacher: printTeacherFunction;
 printTeacher = function (firstName: string, lastName: string) {
   return `${firstName[0]}. ${lastName}`;
 };
+
+interface StundentConstructor {
+  new (firstName: string, lastName: string): StudentInterface;
+}
+
+interface StudentInterface {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    throw new Error('Method not implemented.');
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
