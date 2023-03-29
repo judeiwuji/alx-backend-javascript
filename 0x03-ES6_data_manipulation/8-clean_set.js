0c5c4fc0939ea739b1f79aa9d53bd731e0f2f954
judeiwuji <judeiwuji@gmail.com>
2023-03-29 09:43:15 -0700

    Fixed errors

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,14 +1,14 @@
 export default function cleanSet(set, startString) {
   const cleaned = [];
-  const copy = set;
 
-  for (const [key, value] of set.entries()) {
-    const str = value.substring(0, startString.length);
-    if (startString && str === startString) {
-      const sub = value.substring(startString.length);
-      cleaned.push(sub);
-      copy[key] = sub;
+  for (const value of set) {
+    if (value) {
+      const str = value.substring(0, startString.length);
+      if (startString && str === startString) {
+        const sub = value.substring(startString.length);
+        cleaned.push(sub);
+      }
     }
   }
-  return cleaned.join('-') + "";
+  return cleaned.join('-');
 }
--- 0x03-ES6_data_manipulation/8-main.js
+++ 0x03-ES6_data_manipulation/8-main.js
@@ -6,3 +6,6 @@ console.log(
 console.log(
   cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), ''),
 );
+
+const set = new Set(['id-test', 'id-chicken', 'id-user', , 'id-id-']);
+console.log(cleanSet(set, 'id-'));
