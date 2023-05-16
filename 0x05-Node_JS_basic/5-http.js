441c318c9d01d6dda78f5b5c78ad7af5e74fbda7
judeiwuji <judeiwuji@gmail.com>
2023-05-16 15:48:19 +0100

    Add: Task 5

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,59 @@
+const http = require('http');
+const fs = require('fs');
+
+const database = process.argv[2];
+function getStudentsCount(filename) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filename, { encoding: 'utf-8' }, (err, rawData) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+
+      const rows = rawData.split('\n').filter((d) => d !== '');
+      const columns = rows[0].split(',');
+      const stats = {};
+      for (let i = 1; i < rows.length; i += 1) {
+        const row = rows[i];
+        const data = row.split(',');
+        const student = {};
+        data.forEach((d, i) => {
+          student[columns[i]] = d;
+        });
+        if (stats[student.field]) {
+          stats[student.field].students.push(student.firstname);
+          stats[student.field].count += 1;
+        } else {
+          stats[student.field] = {
+            students: [student.firstname],
+            count: 1,
+            field: student.field,
+          };
+        }
+      }
+      let message = `Number of students: ${rows.length - 1}\n`;
+      for (const stat of Object.values(stats)) {
+        message += `Number of students in ${stat.field}: ${
+          stat.count
+        }. List: ${stat.students.join(', ')}\n`;
+      }
+      resolve(message);
+    });
+  });
+}
+const app = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    const title = 'This is the list of our students';
+    getStudentsCount(database)
+      .then((message) => {
+        res.end(`${title}\n${message}`);
+      })
+      .catch((error) => {
+        res.end(title);
+      });
+  }
+});
+app.listen(1245);
+module.exports = app;
