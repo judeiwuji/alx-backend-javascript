7b408fbc09448826c8bb6ba9636665873f9bc0b4
jude iwuji <judeiwuji@gmail.com>
2023-05-19 17:53:54 +0100

    update

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -4,18 +4,14 @@ const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./3-payment');
 
 describe('sendPaymentRequestToApi', () => {
-  const sandbox = sinon.createSandbox();
+  let utilSpy;
 
   before(() => {
     sinon.stub(console, 'log');
   });
 
   beforeEach(() => {
-    sandbox.spy(Utils);
-  });
-
-  afterEach(() => {
-    sandbox.restore();
+    utilSpy = sinon.spy(Utils, 'calculateNumber');
   });
 
   it('should call calculateNumber function in Utils module', () => {
@@ -23,11 +19,13 @@ describe('sendPaymentRequestToApi', () => {
 
     assert.equal(Utils.calculateNumber.calledOnce, true);
     assert.equal(Utils.calculateNumber.calledWith('SUM', 100, 20), true);
+    utilSpy.restore();
   });
 
   it("should display 'The total is: 120'", () => {
     sendPaymentRequestToApi(100, 20);
 
     assert(console.log.calledWith('The total is: 120'), true);
+    utilSpy.restore();
   });
 });
