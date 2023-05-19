4f94537c96f2677dc64309d4d06679b62f1b096c
jude iwuji <judeiwuji@gmail.com>
2023-05-19 21:25:55 +0100

    Update

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -5,127 +5,127 @@ describe('calculateNumber', () => {
   describe('SUM', () => {
     describe('Integer', () => {
       it('should return 3 for 1, 2', () => {
-        expect(calculateNumber('SUM', 1, 2)).to.equal(3);
+        expect(calculateNumber('SUM', 1, 2)).to.be.equal(3);
       });
 
       it('should return -3 for -1, -2', () => {
-        expect(calculateNumber('SUM', -1, -2)).to.equal(-3);
+        expect(calculateNumber('SUM', -1, -2)).to.be.equal(-3);
       });
 
       it('should return 0 for 0, 0', () => {
-        expect(calculateNumber('SUM', 0, 0)).to.equal(0);
+        expect(calculateNumber('SUM', 0, 0)).to.be.equal(0);
       });
     });
 
     describe('Float', () => {
       it('should return 3 for 1.0, 2.0', () => {
-        expect(calculateNumber('SUM', 1.0, 2.0)).to.equal(3);
+        expect(calculateNumber('SUM', 1.0, 2.0)).to.be.equal(3);
       });
 
       it('should return 6 for 2.5, 2.5', () => {
-        expect(calculateNumber('SUM', 2.5, 2.5)).to.equal(6);
+        expect(calculateNumber('SUM', 2.5, 2.5)).to.be.equal(6);
       });
 
       it('should return 5 for 2.5, 2.3', () => {
-        expect(calculateNumber('SUM', 2.5, 2.3)).to.equal(5);
+        expect(calculateNumber('SUM', 2.5, 2.3)).to.be.equal(5);
       });
 
       it('should return -4 for -2.5, -2.3', () => {
-        expect(calculateNumber('SUM', -2.5, -2.3)).to.equal(-4);
+        expect(calculateNumber('SUM', -2.5, -2.3)).to.be.equal(-4);
       });
     });
 
     describe('Integer and Float', () => {
       it('should return 5 for 1.7, 3', () => {
-        expect(calculateNumber('SUM', 1.7, 3)).to.equal(5);
+        expect(calculateNumber('SUM', 1.7, 3)).to.be.equal(5);
       });
 
       it('should return 1 for -1.7, 3', () => {
-        expect(calculateNumber('SUM', -1.7, 3)).to.equal(1);
+        expect(calculateNumber('SUM', -1.7, 3)).to.be.equal(1);
       });
 
       it('should return -5 for -1.7, -3', () => {
-        expect(calculateNumber('SUM', -1.7, -3)).to.equal(-5);
+        expect(calculateNumber('SUM', -1.7, -3)).to.be.equal(-5);
       });
     });
   });
   describe('SUBTRACT', () => {
     describe('Integer', () => {
       it('should return 1 for 2, 1', () => {
-        expect(calculateNumber('SUBTRACT', 2, 1)).to.equal(1);
+        expect(calculateNumber('SUBTRACT', 2, 1)).to.be.equal(1);
       });
 
       it('should return 1 for -1, -2', () => {
-        expect(calculateNumber('SUBTRACT', -1, -2)).to.equal(1);
+        expect(calculateNumber('SUBTRACT', -1, -2)).to.be.equal(1);
       });
 
       it('should return 0 for 0, 0', () => {
-        expect(calculateNumber('SUBTRACT', 0, 0)).to.equal(0);
+        expect(calculateNumber('SUBTRACT', 0, 0)).to.be.equal(0);
       });
     });
 
     describe('Float', () => {
       it('should return -1 for 1.0, 2.0', () => {
-        expect(calculateNumber('SUBTRACT', 1.0, 2.0)).to.equal(-1);
+        expect(calculateNumber('SUBTRACT', 1.0, 2.0)).to.be.equal(-1);
       });
 
       it('should return 0 for 2.5, 2.5', () => {
-        expect(calculateNumber('SUBTRACT', 2.5, 2.5)).to.equal(0);
+        expect(calculateNumber('SUBTRACT', 2.5, 2.5)).to.be.equal(0);
       });
 
       it('should return 1 for 2.5, 2.3', () => {
-        expect(calculateNumber('SUBTRACT', 2.5, 2.3)).to.equal(1);
+        expect(calculateNumber('SUBTRACT', 2.5, 2.3)).to.be.equal(1);
       });
 
       it('should return 0 for -2.5, -2.3', () => {
-        expect(calculateNumber('SUBTRACT', -2.5, -2.3)).to.equal(0);
+        expect(calculateNumber('SUBTRACT', -2.5, -2.3)).to.be.equal(0);
       });
     });
 
     describe('Integer and Float', () => {
       it('should return -1 for 1.7, 3', () => {
-        expect(calculateNumber('SUBTRACT', 1.7, 3)).to.equal(-1);
+        expect(calculateNumber('SUBTRACT', 1.7, 3)).to.be.equal(-1);
       });
 
       it('should return -5 for -1.7, 3', () => {
-        expect(calculateNumber('SUBTRACT', -1.7, 3)).to.equal(-5);
+        expect(calculateNumber('SUBTRACT', -1.7, 3)).to.be.equal(-5);
       });
 
       it('should return 1 for -1.7, -3', () => {
-        expect(calculateNumber('SUBTRACT', -1.7, -3)).to.equal(1);
+        expect(calculateNumber('SUBTRACT', -1.7, -3)).to.be.equal(1);
       });
     });
   });
   describe('DIVIDE', () => {
     describe('Integer', () => {
       it('should return 0.5 for 1, 2', () => {
-        expect(calculateNumber('DIVIDE', 1, 2)).to.equal(0.5);
+        expect(calculateNumber('DIVIDE', 1, 2)).to.be.equal(0.5);
       });
 
       it('should return 0.5 for -1, -2', () => {
-        expect(calculateNumber('DIVIDE', -1, -2)).to.equal(0.5);
+        expect(calculateNumber('DIVIDE', -1, -2)).to.be.equal(0.5);
       });
 
       it('should return Error for 0, 0', () => {
-        expect(calculateNumber('DIVIDE', 0, 0)).to.equal('Error');
+        expect(calculateNumber('DIVIDE', 0, 0)).to.be.equal('Error');
       });
     });
 
     describe('Float', () => {
       it('should return 0.5 for 1.0, 2.0', () => {
-        expect(calculateNumber('DIVIDE', 1.0, 2.0)).to.equal(0.5);
+        expect(calculateNumber('DIVIDE', 1.0, 2.0)).to.be.equal(0.5);
       });
 
       it('should return 1 for 2.5, 2.5', () => {
-        expect(calculateNumber('DIVIDE', 2.5, 2.5)).to.equal(1);
+        expect(calculateNumber('DIVIDE', 2.5, 2.5)).to.be.equal(1);
       });
 
       it('should return 1.5 for 2.5, 2.3', () => {
-        expect(calculateNumber('DIVIDE', 2.5, 2.3)).to.equal(1.5);
+        expect(calculateNumber('DIVIDE', 2.5, 2.3)).to.be.equal(1.5);
       });
 
       it('should return 1 for -2.5, -2.3', () => {
-        expect(calculateNumber('DIVIDE', -2.5, -2.3)).to.equal(1);
+        expect(calculateNumber('DIVIDE', -2.5, -2.3)).to.be.equal(1);
       });
     });
 
@@ -133,19 +133,19 @@ describe('calculateNumber', () => {
       it('should return 0.67 for 1.7, 3', () => {
         expect(
           parseFloat(calculateNumber('DIVIDE', 1.7, 3).toPrecision(2)),
-        ).to.equal(0.67);
+        ).to.be.equal(0.67);
       });
 
       it('should return -0.6 for -1.7, 3', () => {
         expect(
           parseFloat(calculateNumber('DIVIDE', -1.7, 3).toPrecision(2)),
-        ).to.equal(-0.67);
+        ).to.be.equal(-0.67);
       });
 
       it('should return -0.67 for -1.7, -3', () => {
         expect(
           parseFloat(calculateNumber('DIVIDE', -1.7, -3).toPrecision(2)),
-        ).to.equal(0.67);
+        ).to.be.equal(0.67);
       });
     });
   });
